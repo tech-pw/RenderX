47b07e9b8884185408a88db1262b59ac91e56ec0
Rahul Jindal <rahul.jindal@pw.live>
2024-02-07 16:09:18 +0530

    Rectified the package name to live.pw.pwHtmlLatex

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.latexview"
+    namespace = "live.pw.latexview"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.latexview"
+        applicationId = "live.pw.latexview"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
--- pwhtmllatex/build.gradle.kts
+++ pwhtmllatex/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.customlatex"
+    namespace = "live.pw.pwHtmlLatex"
     compileSdk = 34
 
     defaultConfig {
